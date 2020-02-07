63edffe56281df58b9241d9aa97c8507693a97ca
Daniel Varcas <daniel.varcas@gmail.com>
2020-02-07 22:39:15 +0000

    Add functionality to connect to API, get live event data and display event names on page.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,21 +1,58 @@
 import React from "react";
 import "./App.css";
-import Button from "react-bootstrap/Button";
 import { Navbar } from "react-bootstrap";
 import SkyBetLogo from "./images/skybet-logo.png";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar bg="primary">
-        <Navbar.Brand href="#home">
-          <img src={SkyBetLogo} alt="Sky Bet" />
-        </Navbar.Brand>
-      </Navbar>
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.w = new WebSocket("ws://localhost:8889");
+    this.state = {
+      liveEventsData: {}
+    };
+  }
+
+  componentDidMount() {
+    this.listenForMessages();
+    this.w.onopen = () => this.getLiveEvents();
+  }
+
+  getLiveEvents = (primaryMarkets = false) => {
+    this.w.send(JSON.stringify({ type: "getLiveEvents", primaryMarkets }));
+  };
+
+  listenForMessages() {
+    this.w.addEventListener("message", m => {
+      const message = JSON.parse(m.data);
+      switch (message.type) {
+        case "LIVE_EVENTS_DATA":
+          this.setState({ liveEventsData: message.data });
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  render() {
+    const { liveEventsData } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar bg="primary">
+          <Navbar.Brand href="#home">
+            <img src={SkyBetLogo} alt="Sky Bet" />
+          </Navbar.Brand>
+        </Navbar>
+        <uL>
+          {liveEventsData.length > 0 &&
+            liveEventsData.map(event => (
+              <li key="event.eventId">{event.name}</li>
+            ))}
+        </uL>
+      </div>
+    );
+  }
 }
 
 export default App;
