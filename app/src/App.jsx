45b93fc57e07314e68e37e12cfeb40421a56f1cd
Daniel Varcas <daniel.varcas@gmail.com>
2020-02-07 15:33:25 +0000

    Install bootstrap and react-bootstrap packages. Setup bootstrap sass variable override. Set initial theme colours (subject to change).

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Button from "react-bootstrap/Button";
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,8 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
       </header>
     </div>
   );
