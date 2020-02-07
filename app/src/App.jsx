5d50b3abe10b35271d97042d0ab8f1469a1174a8
Daniel Varcas <daniel.varcas@gmail.com>
2020-02-07 16:56:58 +0000

    Remove create-react-app styles/content. Add SkyBet logo, icons.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,28 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
 import Button from "react-bootstrap/Button";
+import { Navbar } from "react-bootstrap";
+import SkyBetLogo from "./images/skybet-logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="primary">Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-      </header>
+      <Navbar bg="primary">
+        <Navbar.Brand href="#home">
+          <img src={SkyBetLogo} alt="Sky Bet" />
+        </Navbar.Brand>
+      </Navbar>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
     </div>
   );
 }
