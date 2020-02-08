0b50980f6a48281c71c320a1e601ea7fc1aa5c4b
Daniel Varcas <daniel.varcas@gmail.com>
2020-02-08 11:38:07 +0000

    Show live events beneath event type headers.

--- app/src/components/App/App.jsx
+++ app/src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Navbar, ListGroup } from "react-bootstrap";
+import { Navbar, ListGroup, Container, Row, Col } from "react-bootstrap";
 import EventDisplay from "../index";
 import SkyBetLogo from "../../images/skybet-logo.png";
 
@@ -37,6 +37,36 @@ class App extends React.Component {
 
   render() {
     const { liveEventsData } = this.state;
+    const linkedEventTypes =
+      liveEventsData.length > 1
+        ? liveEventsData.map(event => {
+            return {
+              linkedEventTypeId: event.linkedEventTypeId,
+              linkedEventTypeName: event.linkedEventTypeName || "Other Football"
+            };
+          })
+        : [];
+
+    const distinctLinkedEventTypes = linkedEventTypes.reduce(
+      (accumulator, currentValue) => {
+        if (accumulator.length < 1) {
+          accumulator.push(currentValue);
+          return accumulator;
+        }
+
+        if (
+          !accumulator.some(
+            linkedEventType =>
+              linkedEventType.linkedEventTypeId ===
+              currentValue.linkedEventTypeId
+          )
+        ) {
+          accumulator.push(currentValue);
+        }
+        return accumulator;
+      },
+      []
+    );
 
     return (
       <div className="App">
@@ -45,13 +75,52 @@ class App extends React.Component {
             <img src={SkyBetLogo} alt="Sky Bet" />
           </Navbar.Brand>
         </Navbar>
-        <h2>Live Events</h2>
-        <ListGroup as="ul">
-          {liveEventsData.length > 0 &&
-            liveEventsData.map(event => (
-              <EventDisplay key={event.eventId} name={event.name} />
-            ))}
-        </ListGroup>
+        <Container className="my-3">
+          <Row>
+            <Col>
+              <h1>Live Games</h1>
+
+              {distinctLinkedEventTypes.length > 0 &&
+                distinctLinkedEventTypes.map(eventType =>
+                  eventType.linkedEventTypeId ? (
+                    <div key={eventType.linkedEventTypeId} className="my-3">
+                      <h2>{eventType.linkedEventTypeName}</h2>
+                      <ListGroup as="ul">
+                        {liveEventsData.map(event =>
+                          event.linkedEventTypeId ===
+                          eventType.linkedEventTypeId ? (
+                            <EventDisplay
+                              key={event.eventId}
+                              eventId={event.eventId}
+                              name={event.name}
+                            />
+                          ) : null
+                        )}
+                      </ListGroup>
+                    </div>
+                  ) : null
+                )}
+
+              {distinctLinkedEventTypes.length > 0 &&
+                distinctLinkedEventTypes.some(
+                  event => !event.linkedEventTypeId
+                ) && (
+                  <div className="my-3">
+                    <h2>Other Football</h2>
+                    <ListGroup>
+                      {liveEventsData.map(event =>
+                        !event.linkedEventTypeId ? (
+                          <ListGroup.Item key={event.eventId}>
+                            {event.name}
+                          </ListGroup.Item>
+                        ) : null
+                      )}
+                    </ListGroup>
+                  </div>
+                )}
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
--- app/src/components/EventDisplay/EventDisplay.jsx
+++ app/src/components/EventDisplay/EventDisplay.jsx
@@ -3,11 +3,16 @@ import PropTypes from "prop-types";
 import { ListGroup } from "react-bootstrap";
 
 const EventDisplay = props => {
-  const { name } = props;
-  return <ListGroup.Item as="li">{name}</ListGroup.Item>;
+  const { eventId, name } = props;
+  return (
+    <ListGroup.Item as="li" key={eventId}>
+      {name}
+    </ListGroup.Item>
+  );
 };
 
 EventDisplay.propTypes = {
+  eventId: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired
 };
 
